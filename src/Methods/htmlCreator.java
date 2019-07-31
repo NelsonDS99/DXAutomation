9f07f69a2784b5175b49686f15738363655cbaa4
NelsonDS99 <scott_nds@outlook.com>
2019-07-31 15:46:02 -0700

    Error Table Creation
    Successfully bookmarks error in the assay table, still need to format text and add to the top of the page

---
+++ src/Methods/htmlCreator.java
@@ -0,0 +1,266 @@
+package Methods;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Map;
+
+public class htmlCreator extends RevisedVerifyResultMethods
+{
+  public void html (ArrayList<ArrayList<String>> htmlList, String document, Map<String, ArrayList<String>> htmlAnalyte,
+      Map<String, ArrayList<String>> htmlAnalyteD) throws IOException
+  {
+    //Create either html file or txt file
+    String file = path(document); 
+    FileWriter fileWriter = new FileWriter(file);
+    PrintWriter printWriter = new PrintWriter(fileWriter);
+
+    //Create strings
+    
+    final String SampleID                                                   = "Sample ID";
+    final String verifySampleID                                             = "Verification Sample ID";
+    final String verifyAssayName                                            = "Verification Assay Name";
+    final String verifyStatus                                               = "Verification Status";
+    final String verifyErrorStatus                                          = "Verification Error Status";
+    final String verifyAssayNameR                                           = "Verification Right Assay Name";
+    final String verifyTextBox                                              = "Verification Text Result Box";
+    final String verifyResultColor                                          = "Verification of Result Color";
+    final String verifyDisclaimer                                           = "Verification of Disclaimer";
+    final String red                                                        = "FF0000";
+    final String yellow                                                     = "FFFF00";
+
+    final String verifyAnalyteName                                          = "Verification of Analyte Name";
+    final String verifyCt                                                   = "Verification of Ct";
+    final String verifyEndPt                                                = "Verification of End Pt";
+    final String verifyInterp                                               = "Verification of Interpretation Result";
+    final String verifyAnalyteReason                                        = "Verification of Analyte Reason";
+    final String verifyAnalyteResult                                        = "Verification of Analyte Result";
+
+    final String verifyAnalyteDName                                         = "Verification of Analyte Details Name";
+    final String verifyPrbChk1                                              = "Verification of Prb Check 1";
+    final String verifyPrbChk2                                              = "Verification of Prb Check 2";
+    final String verifyPrbChk3                                              = "Verification of Prb Check 3";
+    final String verifyPrbChkResult                                         = "Verification of Probe Check Result"; 
+    final String verifyDerivPeak                                            = "Verification of 2nd Deriv Peak";
+
+    String htmlStarter                                                      = htmlStarter();
+    String htmlEnder                                                        = htmlEnder();
+    String header                                                           = htmlHeader(document);
+    String tableHeaderCells                                                 = tableHeaderCells(SampleID, verifySampleID, verifyAssayName, verifyStatus, verifyErrorStatus, verifyAssayNameR,
+                                                                               verifyTextBox,verifyResultColor, verifyDisclaimer);
+    
+    String tableWidth                                                       = "<table style=\"width:100%\">\r\n<tr>\r\n";
+    String endRow                                                           = "</tr>\r\n";
+    String endTable                                                         = "</table>\r\n";
+    String beginRow                                                         = "<tr>\r\n";
+    String styleTable                                                       = styleTable();
+    String styleRows                                                        = styleRows();
+    String styleExtra                                                       = styleExtra();
+    String errorHeaders                                                     = tableHeaderCells("Sample ID", "Error");
+    String analyteResultsHeaders                                            = tableHeaderCells(verifyAnalyteName,verifyCt, verifyEndPt, verifyInterp, verifyAnalyteReason, verifyAnalyteResult);
+    String analyteDetailHeader                                              = tableHeaderCells(verifyAnalyteDName, verifyPrbChk1, verifyPrbChk2,verifyPrbChk3, verifyPrbChkResult, verifyDerivPeak);
+
+    int errorID = 0;
+    String htmlCode = "";
+    String errorTable = "";
+
+    
+    //Create the table header of Test Results    
+    printWriter.print(htmlStarter);
+    String beginTable = beginTableCreation(header,styleTable,styleRows,styleExtra,tableWidth,tableHeaderCells,endRow); 
+    errorTable = beginTableCreation(styleTable, styleRows,styleExtra,tableWidth,errorHeaders);
+    printWriter.print(beginTable);
+
+    /* Create cells with individual verification results
+     * Iterates through the htmlList for each individual assay
+     * 
+     */
+    for (int outerArray = 0; outerArray < htmlList.size(); outerArray++) {
+      //printWriter.print(beginRow);
+      htmlCode = htmlCode.concat(beginRow); 
+      for (int innerArray = 0; innerArray < htmlList.get(outerArray).size(); innerArray++) {
+        String cell = htmlList.get(outerArray).get(innerArray);       
+        if (cell.contains("Error.")) {
+          //printWriter.print(String.format("<td id=\"Error%s\" style=\"background-color:#%s\"><pre>%s</pre></td>\r\n",errorID, red, cell));
+          htmlCode = htmlCode.concat(String.format("<td id=\"Error%s\" style=\"background-color:#%s\"><pre>%s</pre></td>\r\n",errorID, red, cell));
+         errorTable = errorTable.concat(createErrorCells(htmlList.get(outerArray).get(0), errorID));
+          errorID++;
+        } 
+        else if(cell.contains("***"))
+        {
+        //  printWriter.print(String.format("<td style=\"background-color:#%s\"><pre>%s</pre></td>\r\n", yellow,cell));
+         htmlCode = htmlCode.concat(String.format("<td style=\"background-color:#%s\"><pre>%s</pre></td>\r\n", yellow,cell)); 
+          
+        }
+        else
+          htmlCode = htmlCode.concat(String.format("<td><pre>%s</pre></td>\r\n", cell));
+          //printWriter.print(String.format("<td><pre>%s</pre></td>\r\n", cell));
+      }
+      //printWriter.print(endRow);
+      htmlCode = htmlCode.concat(endRow);
+    }
+
+    //printWriter.print(endTable);
+    htmlCode = htmlCode.concat(endTable);
+    
+    printWriter.print(htmlCode);
+    
+    printWriter.print(errorTable);
+    printWriter.print(endRow);
+    printWriter.print(endTable);
+
+    /*
+     * 
+     * Begin the data creation for the Analytes of each assay
+     * Creates the assay header and table label
+     */
+    printWriter.print("<h2 style=\"text-align:center;\">Analyte Data</h2>\r\n");
+    for (String val : htmlAnalyte.keySet()) {
+      printWriter.print(String.format("<h3><u>%s</u></h3>\r\n", val));
+      printWriter.print(String.format("<h4>%s</h4>\r\n", "Analyte Results Verification"));
+      beginTable = beginTableCreation(styleTable,styleRows,styleExtra,tableWidth);
+      printWriter.print(beginTable);
+      printWriter.print(analyteResultsHeaders);
+      printWriter.print("</tr>\r\n<tr>\r\n");
+
+      /*
+       * 
+       * Iterates through the analyte result map
+       * 0 -- Analyte Name
+       * 1 -- Ct
+       * 2 -- End Pt
+       * 3 -- Interpret Result
+       * 4 -- Analyte reason
+       * 5 -- Analyte Result
+       */
+      for (int count = 0; count < htmlAnalyte.get(val).size(); count++) {
+        
+        //Start a new row once all 6 data points of analyte are checked
+        if (count % 6 == 0 && count != 0) {
+          printWriter.print(endRow);
+        }
+        String aCell = htmlAnalyte.get(val).get(count);
+        if (aCell.contains("Error.")) {
+          printWriter.print(String.format("<td style=\"background-color:#%s\"><pre>%s</pre></td>\r\n", red, aCell));
+        } 
+        else if(aCell.contains("***"))
+        {
+          printWriter.print(String.format("<td style=\"background-color:#%s\"><pre>%s</pre></td>\r\n", yellow,aCell));
+        }
+        else
+          printWriter.print(String.format("<td><pre>%s</pre></td>\r\n", aCell));
+      }
+      printWriter.print(endRow);
+      printWriter.print(endTable);
+
+      //Create the table for analyte details verification
+      printWriter.print(String.format("<h4>%s</h4>\r\n", "Analyte Details Verification"));
+      printWriter.print(beginTable);
+      printWriter.print(analyteDetailHeader);
+      printWriter.print("</tr>\r\n<tr>\r\n");
+      
+      /*
+       * Iterates through the analyte details map
+       * 0 -- Analyte Name
+       * 1 -- Prb Chk 1
+       * 2 -- Prb Chk 2
+       * 3 -- Prb Chk 3
+       * 4 -- Probe Check Result
+       * 5 -- 2nd Deriv Peak
+       * 
+       */
+      for (int detailNum = 0; detailNum < htmlAnalyteD.get(val).size(); detailNum++) {
+        if (detailNum % 6 == 0 && detailNum != 0) {
+          printWriter.print(endRow);
+        }
+        String aCell = htmlAnalyteD.get(val).get(detailNum);
+        if (aCell.contains("Error."))
+          printWriter.print(String.format("<td style=\"background-color:#%s\"><pre>%s</pre></td>\r\n", red, aCell));
+        else if(aCell.contains("***"))
+         printWriter.print(String.format("<td style=\"background-color:#%s\"><pre>%s</pre></td>\r\n", yellow,aCell));
+        else
+          printWriter.print(String.format("<td><pre>%s</pre></td>\r\n", aCell));
+
+      }
+      printWriter.print(endRow);
+      printWriter.print(endTable);
+    }
+
+    //Finish the html code
+    printWriter.print(htmlEnder);
+    printWriter.close();
+    fileWriter.close();
+
+  }
+  
+  private String path(String document)
+  {
+    return String.format(
+        "C:\\Users\\nelson.scott\\gitDx\\GeneXpertUiAuto\\%s.html",
+        document);
+  }
+  private String htmlStarter()
+  {
+    return "<!DOCTYPE html>\r\n<html>\r\n<head>\r\n<title>Verification Results</title>\r\n</head>\r\n<body>\r\n";
+  }
+  
+  private String htmlEnder()
+  {
+    return "</body>\r\n</html>";
+  }
+  
+  private String htmlHeader(String document)
+  {
+    return String.format("<h1 style= \"text-align:center;\">%s</h1>\r\n", document);
+  }
+  
+  private String tableHeaderCells(String ... args)
+  {
+    String concat = ""; 
+    for(String arg: args)
+    {
+      concat = concat.concat(String.format("<th>%s</th>\r\n", arg));
+    }
+    return concat;
+  }
+
+  private String styleTable()
+  {
+    return "<style>\r\ntable {\r\n border-collapse: collapse; \r\n } \r\n";
+  }
+  
+  private String styleRows()
+  {
+    return "th,td {\r\nborder: 1px solid green; \r\n } \r\n ";
+  }
+  private String styleExtra()
+  {
+    return "tr:nth-child(even) { \r\n background-color: #eee; \r\n } \r\n tr:nth-child(odd) { \r\n background-color: #fff; \r\n } \r\n </style> \r\n";
+  }
+  
+  private String beginTableCreation(String...args)
+  {
+    String concat = "";
+    
+    for(String arg: args)
+    {
+      concat = concat.concat(arg);
+    }
+    
+    return concat;
+  }
+  private String createErrorCells(String assay, int error)
+  {
+    String errorCell = ""; 
+    
+    errorCell = String.format("<tr>\r\n"
+        + "<td><pre>%s</pre></td>\r\n"
+        + "<td><pre><a href=\"#Error%s\">Jump to Error</a></pre></td>\r\n"
+        + "</tr>\r\n", assay, error);
+    
+    return errorCell;
+  }
+  
+  
+}
---
+++ src/test/CTNGTestResults.java
@@ -0,0 +1,141 @@
+package test;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+
+import org.junit.Test;
+import Methods.htmlCreator;
+
+public class CTNGTestResults extends htmlCreator
+{
+  @Test
+  public void CTNGTestResultsVerification ()
+  {
+    String document = "D6781 CTNG Test Results Verification";
+    FileInputStream file = openFileInputStream(String.format(
+        "C:\\Silk Data Files\\D6781 Silk Data Files\\%s.xls",document));
+
+    // Get excel workbook && Get Specific Sheet
+    HSSFWorkbook workbook = openHSSFWorkbook(file);
+    HSSFSheet sheet = openHSSFSheet(workbook, "AllTestResults");
+
+    // Login to Cepheid
+   // userLogin();
+    // View Test Panel
+    navigateToViewResults();
+
+    Map<String, ArrayList<String>> expectedData = createStaticMapValues(sheet);
+
+    Map<String, Integer> sampleIDList = actualResultsList(expectedData);
+    
+    ArrayList<ArrayList<String>> htmlData = new ArrayList<ArrayList<String>>();
+    ArrayList<String> htmlValue; 
+    
+    Map<String, ArrayList<String>> htmlAnalyteR = new HashMap<String,ArrayList<String>>();
+    ArrayList<String> htmlARValue;
+    
+    Map<String,ArrayList<String>>  htmlAnalyteD = new HashMap<String,ArrayList<String>>(); 
+    ArrayList<String> htmlADValue;
+
+    int count = 0;
+
+    for (String key : sampleIDList.keySet()) {
+
+      //Add key to htmlData
+      htmlValue = new ArrayList<String>();
+      htmlValue.add(key); 
+      //Analyte Results Storage
+      htmlARValue = new ArrayList<String>(); 
+      
+      htmlADValue = new ArrayList<String>(); 
+      
+      // Click Specific Assay in JTable
+      clickResult(sampleIDList.get(key));
+
+      // Check left GUI panel
+      htmlValue.addAll(verifyGuiLeftPanel(expectedData, key));
+
+      // Check error status text in support tab
+      htmlValue.addAll(verifyErrorStatus(expectedData, key));
+
+      // Check right assay name in GUI
+      htmlValue.add(verifyRightAssayName(expectedData, key));
+
+      ArrayList<String> resultText = resultText(sheet, key);
+
+      // Check the text in the textbox of GUI
+      htmlValue.add(verifyTestResultBox(expectedData, key, resultText));
+
+      ArrayList<String> resultBGColor = resultBGColor(sheet, key);
+
+      ArrayList<String> resultFontColor = resultFontColor(sheet, key);
+
+      ArrayList<Integer> resultPos = resultPos(sheet, key);
+
+      // Check the background color and font color of textbox
+      htmlValue.add(verifyColor(expectedData, key, resultBGColor, resultFontColor, resultPos));
+      
+      //Check the disclaimer box
+      String resultDisclaimer = resultDisclaimer(sheet, key); 
+      htmlValue.add(verifyDisclaimer(resultDisclaimer)); 
+
+      HSSFSheet analyteSheet = openHSSFSheet(workbook, key);
+
+      // Create map of Analyte Data for each assay
+      Map<String, ArrayList<String>> analyteDataMap = exAnalyteDataMap(analyteSheet);
+       
+      logInfo(Arrays.toString(analyteDataMap.keySet().toArray()));
+      int rowNum = 0;
+
+      clickTabbedPane(VR_TABBED_PANE, "Analyte Result");
+
+      // Check the analyte results tab
+      for (String analyte : analyteDataMap.keySet()) {
+       
+        if (rowNum < findJTable(VR_ANALYTE_RTABLE).getRowCount()) {
+          htmlARValue.addAll(verifyAnalyteResults(analyteDataMap, rowNum));
+        }
+        rowNum++;
+      }
+
+      // Check the analyte details tab
+      if (key.equals("ORG-12.A4") || key.equals("ORG-12.A3")) {
+        rowNum = 1;
+      } else
+        rowNum = 0;
+
+      clickTabbedPane(VR_TABBED_PANE, "Detail");
+      for (String analyte : analyteDataMap.keySet()) {
+        
+        htmlADValue.addAll(verifyAnalyteDetails(analyteDataMap, rowNum));
+
+        rowNum++;
+      }
+
+      count++;
+      
+      //Add to htmlArrayList
+     htmlData.add(htmlValue);
+     htmlAnalyteR.put(key, htmlARValue); 
+     htmlAnalyteD.put(key, htmlADValue); 
+
+      logInfo("====================");
+    }
+    try {
+      html(htmlData,document, htmlAnalyteR,htmlAnalyteD);
+    } catch (IOException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
+    logInfo(String.format("I analyzed %s test results", count));
+  }
+ 
+
+}
